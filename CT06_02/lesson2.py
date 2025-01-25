2964deb841fde94f4d9aac18ea8371a3eb91da68
Computhink-CS126 <cs126@computhink.com.sg>
2025-01-25 06:07:01 +0000

    Jan 25, 2025, 6:07 AM

--- CT06_02/lesson2.py
+++ CT06_02/lesson2.py
@@ -1,7 +1,69 @@
 print("Hello from lesson 2")
+print("Red")
+print("Orange")
+print("Yellow")
+print("Green")
+print("blue")
+print("Indigo")
+print("Violet")
+
+
+
+# ask user for age
+# store user age to "answer"
+# if "answer" is below "min_age"
+#      say "access denied"
+# else 
+#      say "welcome!"
+
+# repeat 10 times
+    #print "Hey!"
+    #move 10
+
+
+
+# set "counter" to 0
+# repeat until "counter" = 50
+    # move 10 steps to the right
+    # turn 15 degrees to the right
+    # change "counter" by 10
+
+
+# ask "What is your age?"
+# store answer into "age"
+# if "answer" is below 18
+    # print "Access Denied!"
+# Else print "Welcome!"
+
+
+
 
 ######## Write the pseudocode in comments for task 2 here
 # Using comments, translate the code shown on screen into pseudocode.
 
+# Check item material
+# if Item consists of metal
+    # add to "metal"
+# else if item consists of Plastic
+    # add material to 
+
+
 ######## Write the pseudocode in comments for task 3 here
 # Using comments, translate the code shown on screen into pseudocode.
+
+# Create variable "answer"
+# Create variable "Secret_Phrase"
+# ask "What is the secret phrase?" and add response to "answer"
+    # if "answer" = "Secret_Phrase"
+        # print "Congratulations!"
+    # Else
+        # print " Access Denied! Cause: Wrong answer "
+
+# Create Variable "Test1"
+# Create Variable "Test2"
+# Create Variable "Test3"
+# Create Variable "Total_Score"
+# Multiply "Test1" by 20% And store answer to "Test1"
+# Multiply "Test2" by 40% And store answer to "Test2"
+# Multiply "Test3" by 40% And store answer to "Test3"
+# Add "Test1" "Test2" "Test3" and store answer to "Total_Score" 
