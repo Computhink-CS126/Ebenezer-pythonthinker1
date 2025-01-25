de57109cb6ae20be160223e85b6dde4a10918da1
Computhink-CS126 <cs126@computhink.com.sg>
2025-01-25 06:21:54 +0000

    Jan 25, 2025, 6:21 AM

--- CT06_03/lesson3.py
+++ CT06_03/lesson3.py
@@ -2,5 +2,5 @@ print("Hello from lesson 3")
 
 # Create variable Adam = 60
 # Create variable Brandon = 58
-# Create variable Charlie = 
-# Create variable Daniel
+# Create variable Charlie = 98
+# Create variable Daniel = 79
