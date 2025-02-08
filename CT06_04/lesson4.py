9838fd3c4818ebd96946bcc6582771933b015168
Computhink-CS126 <cs126@computhink.com.sg>
2025-02-08 06:49:14 +0000

    Feb 8, 2025, 6:49 AM

--- CT06_04/lesson4.py
+++ CT06_04/lesson4.py
@@ -7,4 +7,4 @@ Red_Plates = 3
 Blue_Plates = 5
 Green_Plates = 4
 Total_Cost = (Red_Plates * Red_Price) + (Blue_Plates * Blue_Price) + (Green_Plates * Green_Price)
-print(Total_Cost)
+Print(Total_Cost)
