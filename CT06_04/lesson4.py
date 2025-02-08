f764cd90473da4903bbdd3cdc5ba19e904c3fb15
Computhink-CS126 <cs126@computhink.com.sg>
2025-02-08 06:59:23 +0000

    Feb 8, 2025, 6:59 AM

--- CT06_04/lesson4.py
+++ CT06_04/lesson4.py
@@ -1,13 +1,13 @@
-print("Hello from lesson 4")
+# print("Hello from lesson 4")
 
-Red_Price = 1
-Blue_Price = 2
-Green_Price = 3
-Red_Plates = 3
-Blue_Plates = 5
-Green_Plates = 4
-Total_Cost = (Red_Plates * Red_Price) + (Blue_Plates * Blue_Price) + (Green_Plates * Green_Price)
-print(Total_Cost)
+# Red_Price = 1
+# Blue_Price = 2
+# Green_Price = 3
+# Red_Plates = 3
+# Blue_Plates = 5
+# Green_Plates = 4
+# Total_Cost = (Red_Plates * Red_Price) + (Blue_Plates * Blue_Price) + (Green_Plates * Green_Price)
+# print(Total_Cost)
 
 
 
