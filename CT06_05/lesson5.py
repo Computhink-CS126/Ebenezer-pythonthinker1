20d65bdcca9d7a3b12a456b1ff196c54ced8e184
Computhink-CS126 <cs126@computhink.com.sg>
2025-02-16 03:33:16 +0000

    Feb 16, 2025, 3:33 AM

--- CT06_05/lesson5.py
+++ CT06_05/lesson5.py
@@ -7,7 +7,9 @@
 # print("your age is " + str(Age))
 
 
-Name = input("what is the birthday person's name?")
-Age = int(input("what age is he/she turning this year?"))
-Personal_msg = input("What is the message you want to send him/her?")
-print("Happy " + str(Age) + "th Birthday " + Name + "!" + Personal_msg)
+# Name = input("what is the birthday person's name?")
+# Age = int(input("what age is he/she turning this year?"))
+# Personal_msg = input("What is the message you want to send him/her?")
+# print("Happy " + str(Age) + "th Birthday " + Name + "!" + Personal_msg)
+
+
