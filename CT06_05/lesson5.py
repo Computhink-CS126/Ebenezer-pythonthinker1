9407073618ddfc5282f5bde1a9ff645413717e23
Computhink-CS126 <cs126@computhink.com.sg>
2025-02-16 03:14:01 +0000

    Feb 16, 2025, 3:14 AM

--- CT06_05/lesson5.py
+++ CT06_05/lesson5.py
@@ -1,3 +1,4 @@
-number = int(input("Give me a number."))
-double_Num = number * 2
-print("double your number is " + (str(double_Num)))
+# number = int(input("Give me a number."))
+# double_Num = number * 2
+# print("double your number is " + (str(double_Num)))
+
