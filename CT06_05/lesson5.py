4e7c0e8013b481aa17840a85e1ef9d2b1ef52181
Computhink-CS126 <cs126@computhink.com.sg>
2025-02-16 03:23:10 +0000

    Feb 16, 2025, 3:23 AM

--- CT06_05/lesson5.py
+++ CT06_05/lesson5.py
@@ -2,8 +2,8 @@
 # double_Num = number * 2
 # print("double your number is " + (str(double_Num)))
 
-year_born = int(input("what year were you born?"))
-Age = 2025 - year_born
-print("your age is " + str(Age))
+# year_born = int(input("what year were you born?"))
+# Age = 2025 - year_born
+# print("your age is " + str(Age))
 
 
