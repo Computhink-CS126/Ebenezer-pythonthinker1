6ba2d644adf04c05734e531278321b47f15e7dc5
Computhink-CS126 <cs126@computhink.com.sg>
2025-02-22 07:20:23 +0000

    Feb 22, 2025, 7:20 AM

--- CT06_06/lesson6.py
+++ CT06_06/lesson6.py
@@ -1,16 +1,16 @@
-# **Task 1a**:
-for i in range(3):
-    print("Hello, World!")
+# # **Task 1a**:
+# for i in range(3):
+#     print("Hello, World!")
 
-# **Task 1b**:
-for i in range(5):
-  print(i)
+# # **Task 1b**:
+# for i in range(5):
+#   print(i)
 
-# **Task 1c**:
-print("Hello, World!")
+# # **Task 1c**:
+# print("Hello, World!")
 
-# **Task 1d**:
-i = 5
+# # **Task 1d**:
+# i = 5
 
-# **Task 1e**:
-print ("Hello, World!")
+# # **Task 1e**:
+# print ("Hello, World!")
