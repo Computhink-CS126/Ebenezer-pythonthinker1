12a03af78baef7630034915584ac162b63b301b2
Computhink-CS126 <cs126@computhink.com.sg>
2025-03-01 06:35:53 +0000

    Mar 1, 2025, 6:35 AM

--- CT06_07/lesson7.py
+++ CT06_07/lesson7.py
@@ -22,5 +22,9 @@
 # print("Average score for " + student_name + " is: " + str(average_score))
 
 
-for i in range(1, 11):
+# for i in range(1, 11):
+#     print(i)
+
+
+for i in range(2, 20, 2):
     print(i)
