e23eb7a9a2954203ceb2b91929250c1eec377d71
Computhink-CS126 <cs126@computhink.com.sg>
2025-03-01 06:31:57 +0000

    Mar 1, 2025, 6:31 AM

--- CT06_07/lesson7.py
+++ CT06_07/lesson7.py
@@ -17,6 +17,6 @@ average_score = total / 3
 
 student_name = "Alex"
 
-average_score = float(total)
+average_score = float(average_score)
 
 print("Average score for " + student_name + " is: " + str(average_score))
