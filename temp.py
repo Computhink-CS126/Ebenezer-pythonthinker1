64bf95339db11af97e16833a248d77b53c2718e1
Computhink-CS126 <cs126@computhink.com.sg>
2025-03-09 08:43:48 +0000

    Mar 9, 2025, 8:43 AM

--- temp.py
+++ temp.py
@@ -2,17 +2,17 @@
 # print("Nice to meet you, " + name + "!")
 
 
-start = int(input("What is the starting number? "))
-stop = int(input("What is the ending number? "))
-if stop > start:
-    stop = stop + 1
-else:
-    stop = stop - 1
+# start = int(input("What is the starting number? "))
+# stop = int(input("What is the ending number? "))
+# if stop > start:
+#     stop = stop + 1
+# else:
+#     stop = stop - 1
 
-Increment = int(input("What is the Increment? "))
+# Increment = int(input("What is the Increment? "))
 
-for i in range(start, stop, Increment):
-    print(i)
+# for i in range(start, stop, Increment):
+#     print(i)
 
 
 print(5 + "John")
