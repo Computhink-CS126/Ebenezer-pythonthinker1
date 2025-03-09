a5a366b4c93d28d8209e6e9bb6161416bcf2e89f
Computhink-CS126 <cs126@computhink.com.sg>
2025-03-09 08:37:12 +0000

    Mar 9, 2025, 8:37 AM

--- temp.py
+++ temp.py
@@ -6,7 +6,9 @@ start = int(input("What is the starting number?"))
 Increment = int(input("What is the Increment?"))
 stop = int(input("What is the ending number?"))
 if stop > start:
-    
+    stop = stop + 1
+else:
+    stop
 
 for i in range(start, Stop, Increment):
     print(i)
