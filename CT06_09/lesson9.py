b3fd41fed1448486cc6660ad5722bf5a6ff698dd
Computhink-CS126 <cs126@computhink.com.sg>
2025-03-16 08:53:33 +0000

    Mar 16, 2025, 8:53 AM

--- CT06_09/lesson9.py
+++ CT06_09/lesson9.py
@@ -1,5 +1,5 @@
 import random
-while yos = false:
+while yos = False:
     num1 = random.randint(1, 6)
     num2 = random.randint(1, 6)
     num3 = random.randint(1, 6)
@@ -7,7 +7,7 @@ while yos = false:
     num22 = ( num2 % 2 ) == 0
     num33 = ( num2 % 2 ) == 0
     yos = num11 == num22 == num33  
-yos = str(yos)
+    yos = str(yos)
     print("the first number is: " + str(num1))
     print("the second number is: " + str(num2))
     print("the third number is: " + str(num3))
