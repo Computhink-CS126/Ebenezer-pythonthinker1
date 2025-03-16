390067a088d37a16d801f46ff6405c8d427e4c06
Computhink-CS126 <cs126@computhink.com.sg>
2025-03-16 08:35:52 +0000

    Mar 16, 2025, 8:35 AM

--- CT06_09/lesson9.py
+++ CT06_09/lesson9.py
@@ -2,4 +2,8 @@ import random
 num1 = random.randint(1, 6)
 num2 = random.randint(1, 6)
 num3 = random.randint(1, 6)
-print("All numbers are even/odd: ")
+num1 = ( num1 % 2 ) == 0
+num2 = ( num2 % 2 ) == 0
+num3 = ( num2 % 2 ) == 0
+boolen = num1 == num2 == num3
+print("All numbers are even/odd: " + boolen)
