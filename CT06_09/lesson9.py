1c5fb16c4c97b27a9de3293fdec9dbe16fa33e1a
Computhink-CS126 <cs126@computhink.com.sg>
2025-03-16 08:47:25 +0000

    Mar 16, 2025, 8:47 AM

--- CT06_09/lesson9.py
+++ CT06_09/lesson9.py
@@ -2,9 +2,9 @@ import random
 num1 = random.randint(1, 6)
 num2 = random.randint(1, 6)
 num3 = random.randint(1, 6)
-num1 = ( num1 % 2 ) == 0
-num2 = ( num2 % 2 ) == 0
-num3 = ( num2 % 2 ) == 0
+num11 = ( num1 % 2 ) == 0
+num22 = ( num2 % 2 ) == 0
+num33 = ( num2 % 2 ) == 0
 yos = num1 == num2 == num3
 yos = str(yos)
 print("All numbers are even/odd: " + yos)
