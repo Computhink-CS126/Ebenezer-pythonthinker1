021a328dc658e43c3738ba68e485207c139dfcda
Computhink-CS126 <cs126@computhink.com.sg>
2025-03-16 09:15:30 +0000

    Mar 16, 2025, 9:15 AM

--- CT06_09/lesson9.py
+++ CT06_09/lesson9.py
@@ -24,10 +24,6 @@ import random
 
 randnum = str(random.randint(1, 10))
 guess = (input("Guess a number between 1 and 10: "))
-if guess == int:
-    guess = (guess)
-else:
-    guess = str(guess)
 if guess == randnum:
     print("CORRECT!>!>!$JU@YU*UDUYD&U*@")
 elif guess == "archangel here":
