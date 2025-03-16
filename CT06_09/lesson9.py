e51cc85bd9e4f597e3fae1a250a4c5962402920c
Computhink-CS126 <cs126@computhink.com.sg>
2025-03-16 08:36:07 +0000

    Mar 16, 2025, 8:36 AM

--- CT06_09/lesson9.py
+++ CT06_09/lesson9.py
@@ -5,5 +5,5 @@ num3 = random.randint(1, 6)
 num1 = ( num1 % 2 ) == 0
 num2 = ( num2 % 2 ) == 0
 num3 = ( num2 % 2 ) == 0
-boolen = num1 == num2 == num3
-print("All numbers are even/odd: " + boolen)
+yos = num1 == num2 == num3
+print("All numbers are even/odd: " + yos)
