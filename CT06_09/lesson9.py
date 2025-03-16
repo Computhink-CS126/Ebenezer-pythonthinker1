93d66a9d1e2f50d56f2b4cb850b2ef39f9484e43
Computhink-CS126 <cs126@computhink.com.sg>
2025-03-16 09:05:30 +0000

    Mar 16, 2025, 9:05 AM

--- CT06_09/lesson9.py
+++ CT06_09/lesson9.py
@@ -1,18 +1,21 @@
-import random
-yos = False
-while yos != True:
-    num1 = random.randint(1, 6)
-    num2 = random.randint(1, 6)
-    num3 = random.randint(1, 6)
-    num11 = ( num1 % 2 ) == 0
-    num22 = ( num2 % 2 ) == 0
-    num33 = ( num2 % 2 ) == 0
-    yos = num11 == num22 == num33  
-    yos = str(yos)
-    print("the first number is: " + str(num1))
-    print("the second number is: " + str(num2))
-    print("the third number is: " + str(num3))
-    print("All numbers are even/odd: " + yos)
+# import random
+# yos = False
+# while yos != True:
+#     num1 = random.randint(1, 6)
+#     num2 = random.randint(1, 6)
+#     num3 = random.randint(1, 6)
+#     num11 = ( num1 % 2 ) == 0
+#     num22 = ( num2 % 2 ) == 0
+#     num33 = ( num2 % 2 ) == 0
+#     yos = num11 == num22 == num33  
+#     yos = str(yos)
+#     print("the first number is: " + str(num1))
+#     print("the second number is: " + str(num2))
+#     print("the third number is: " + str(num3))
+#     print("All numbers are even/odd: " + yos)
+
+
+
 
 
 
