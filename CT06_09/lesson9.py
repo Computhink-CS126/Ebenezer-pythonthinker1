1538adb0ba2850bafee961f49aa1eceb94b18578
Computhink-CS126 <cs126@computhink.com.sg>
2025-03-16 09:23:22 +0000

    Mar 16, 2025, 9:23 AM

--- CT06_09/lesson9.py
+++ CT06_09/lesson9.py
@@ -1,18 +1,18 @@
-import random
-# yos = False
-# while yos != True:
-#     num1 = random.randint(1, 6)
-#     num2 = random.randint(1, 6)
-#     num3 = random.randint(1, 6)
-#     num11 = ( num1 % 2 ) == 0
-#     num22 = ( num2 % 2 ) == 0
-#     num33 = ( num2 % 2 ) == 0
-#     yos = num11 == num22 == num33  
-#     yos = str(yos)
-#     print("the first number is: " + str(num1))
-#     print("the second number is: " + str(num2))
-#     print("the third number is: " + str(num3))
-#     print("All numbers are even/odd: " + yos)
+# import random
+# # yos = False
+# # while yos != True:
+# #     num1 = random.randint(1, 6)
+# #     num2 = random.randint(1, 6)
+# #     num3 = random.randint(1, 6)
+# #     num11 = ( num1 % 2 ) == 0
+# #     num22 = ( num2 % 2 ) == 0
+# #     num33 = ( num2 % 2 ) == 0
+# #     yos = num11 == num22 == num33  
+# #     yos = str(yos)
+# #     print("the first number is: " + str(num1))
+# #     print("the second number is: " + str(num2))
+# #     print("the third number is: " + str(num3))
+# #     print("All numbers are even/odd: " + yos)
 
 
 
@@ -29,14 +29,14 @@ import random
 
 
 
-randnum = str(random.randint(1, 10))
-guess = str(input("Guess a number between 1 and 10: "))
-if guess == randnum:
-    print("CORRECT!>!>!$JU@YU*UDUYD&U*@")
-elif guess == "archangel Gabriel here":
-    print("Welcome archangel Gabriel")
-else:
-    print("You have gotten the wrong answer. :) you will now die 😁😁😁😁😁😁😁😁😁😁😁😐")
+# randnum = str(random.randint(1, 10))
+# guess = str(input("Guess a number between 1 and 10: "))
+# if guess == randnum:
+#     print("CORRECT!>!>!$JU@YU*UDUYD&U*@😐")
+# elif guess == "archangel Gabriel here":
+#     print("Welcome archangel Gabriel")
+# else:
+#     print("You have gotten the wrong answer. :) you will now die 😁😁😁😁😁😁😁😁😁😁😁😐")
 
 
 
