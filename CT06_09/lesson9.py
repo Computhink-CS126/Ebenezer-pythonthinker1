726d122c49e7e62a0f0720e7903062ee99ec5b65
Computhink-CS126 <cs126@computhink.com.sg>
2025-03-16 08:48:06 +0000

    Mar 16, 2025, 8:48 AM

--- CT06_09/lesson9.py
+++ CT06_09/lesson9.py
@@ -7,5 +7,7 @@ num22 = ( num2 % 2 ) == 0
 num33 = ( num2 % 2 ) == 0
 yos = num1 == num2 == num3
 yos = str(yos)
-print("the first number is: " + str(num11)
+print("the first number is: " + str(num11))
+print("the first number is: " + str(num11))
+print("the first number is: " + str(num11))
 print("All numbers are even/odd: " + yos)
