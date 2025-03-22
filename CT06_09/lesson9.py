45e4db2ea2ff0f03a8a1d48086ca0f0e96482c20
Computhink-CS126 <cs126@computhink.com.sg>
2025-03-22 07:30:09 +0000

    Mar 22, 2025, 7:30 AM

--- CT06_09/lesson9.py
+++ CT06_09/lesson9.py
@@ -114,4 +114,6 @@ total = 0
 num_oranges = int(input("how many oranges do you wanna buy?"))
 num_apples = int(input("how many apples do u wanna buy?"))
 if num_apples > 5:
-    cost_Apple
+    cost_Apple = num_apples * 0.9
+if num_oranges > 5:
+    cost
