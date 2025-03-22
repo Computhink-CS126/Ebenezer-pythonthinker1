6e858ef57b434ee09cfc79ea3e34844d3cf6897e
Computhink-CS126 <cs126@computhink.com.sg>
2025-03-22 06:22:43 +0000

    Mar 22, 2025, 6:22 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -4,4 +4,4 @@ guess = int(input("Guess a number between 1 and 10."))
 if guess == ans:
     print("Congratulations! your answer is correct :)")
 else:
-    print("")
+    print("Your answer was incorrect, the correct answer was ")
