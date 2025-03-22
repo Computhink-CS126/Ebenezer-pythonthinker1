01000101282d980d15d52c10ee88de16b755bb37
Computhink-CS126 <cs126@computhink.com.sg>
2025-03-22 07:33:39 +0000

    Mar 22, 2025, 7:33 AM

--- CT06_09/lesson9.py
+++ CT06_09/lesson9.py
@@ -117,4 +117,5 @@ if num_apples > 5:
     cost_Apple = num_apples * 0.9
 if num_oranges > 5:
     cost_Orange = num_oranges * 0.9
-total = (cost_Apple * num_apples) + (cost_Orange * num_)
+total = (cost_Apple * num_apples) + (cost_Orange * num_oranges)
+print("Your total will be " + total + ".")
